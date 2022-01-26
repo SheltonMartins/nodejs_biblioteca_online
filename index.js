b6e6f23cf162e170201e01ade8f0fc698c9655d4
Shelton Martins <sheltonmartinslucas@gmail.com>
2022-01-26 11:19:27 -0300

    Editando os dados do DB

--- index.js
+++ index.js
@@ -20,6 +20,29 @@ app.get ('/', (req,res)=>{
 
 })
 
+app.get('/books/edit/:id',(req,res)=>{
+
+    const id = req.params.id
+
+    const sql = `SELECT * FROM books WHERE id= ${id}`
+
+    conn.query(sql, function(err, data){
+        if(err){
+            console.log(err)
+            return
+        }
+        const book = data[0]
+
+        res.render('edit', {book:book})
+        console.log(book)
+
+    })
+
+})
+
+
+
+
 app.get('/books/:id',(req,res)=>{
 
     const id = req.params.id
@@ -39,6 +62,7 @@ app.get('/books/:id',(req,res)=>{
 
 })
 
+
 app.get('/books', (req,res)=>{
     const sql = "SELECT * FROM books"
 
@@ -51,8 +75,6 @@ app.get('/books', (req,res)=>{
 
         const books = data
 
-        //console.log(books)
-
         res.render('books', {books})
     })
 
@@ -64,6 +86,24 @@ app.post('/books/insertbook', (req, res)=>{
     const pageqty = req.body.pageqty
     const sql = `INSERT INTO books(title, pageqty) VALUES('${title}','${pageqty}')` 
 
+    conn.query(sql, function(err){
+        if(err){
+            console.log(err)
+            return
+        }
+        res.redirect('/books')
+
+    })
+
+})
+
+app.post('/books/updatebook', (req, res)=>{
+
+    const id = req.body.id    
+    const title = req.body.title
+    const pageqty = req.body.pageqty
+    const sql = `UPDATE books SET pageqty='${pageqty}', title='${title}' WHERE id=${id}` 
+    
     conn.query(sql, function(err){
         if(err){
             console.log(err)
